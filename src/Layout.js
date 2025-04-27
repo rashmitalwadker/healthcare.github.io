1934339340c32351651245deb692b45d6421978b
rashmi talwadker <you@example.com>
2025-04-27 19:50:59 +0530

    react app

---
+++ src/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <div>
+    <header>
+    <img src=""/>    
+    </header>
+    <main></main> 
+    <bottom></bottom>   
+    </div>
+  )
+}
+
+export default Layout
